f77e16e05e08075f8bb498682e53f078d06dc267
Nikita Kondratev <highflyer16@yandex.ru>
2026-02-22 14:12:28 +0300

    Refactor. Remove old apps proxy and telegram bot

--- proxy/services/utils.js
+++
@@ -1,6 +0,0 @@
-import { dirname as stdlibDirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-export function dirname (fileURL) {
-  return stdlibDirname(fileURLToPath(fileURL))
-}
